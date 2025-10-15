aa05488190cc3686f23930f63a8c7bea13fcde00
skjsjhb <skjsjhb@outlook.com>
2025-10-15 11:16:13 +0800

    Add ICS-2

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,8 @@
 import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 const config: Config = {
     title: 'ICS 2025 Miao',
@@ -40,6 +42,8 @@ const config: Config = {
                     // Remove this to remove the "edit this page" links.
                     // editUrl:
                     //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                    remarkPlugins: [remarkMath],
+                    rehypePlugins: [rehypeKatex],
                 },
                 theme: {
                     customCss: './src/css/custom.css',
@@ -117,6 +121,16 @@ const config: Config = {
             darkTheme: prismThemes.vsDark,
         },
     } satisfies Preset.ThemeConfig,
+
+    stylesheets: [
+        {
+            href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+            type: 'text/css',
+            integrity:
+                'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+            crossorigin: 'anonymous',
+        },
+    ],
 };
 
 export default config;
